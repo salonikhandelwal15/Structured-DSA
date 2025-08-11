2a1df702b23a70634c2b56baf98923fd3a838e9a
Saloni <salonikhandelwal22553@gmail.com>
2025-08-11 20:11:16 +0530

    Solved a problem of Frequency of each element in an integer

---
+++ Arrays/src/Frequency.java
@@ -0,0 +1,20 @@
+import java.util.HashMap;
+import java.util.Map;
+public class Frequency {
+    public static void main(String[] args) {
+        int []arr={1,4,5,9,7,7,1,4,5};
+        Map<Integer,Integer>hm=new HashMap<>();
+        for(int i=0;i<arr.length;i++){
+            if(hm.containsKey(arr[i])){
+                hm.put(arr[i],hm.get(arr[i])+1);
+            }
+            else{
+                hm.put(arr[i],1);
+            }
+        }
+        for(Map.Entry entry :hm.entrySet()){
+            System.out.println("Element | Frequency");
+            System.out.println(entry.getKey()+"  "+entry.getValue());
+        }
+    }
+}
