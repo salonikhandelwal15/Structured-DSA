f61c0a2d96a9a1173440e6ba10dbca8bc2f957b1
Saloni <salonikhandelwal22553@gmail.com>
2025-08-12 22:38:04 +0530

    Solved an integer which repeatedly add all its digits untill the result has only one digit

---
+++ Arrays/src/AddDigitsUntilGetsDigitOne.java
@@ -0,0 +1,14 @@
+
+public class AddDigitsUntilGetsDigitOne {
+    public static void main(String[] args) {
+        int num = 38;
+        System.out.println(addDigits(num));
+    }
+
+    public static int addDigits(int num) {
+        if (num == 0) {
+            return 0;
+        }
+        return (num % 9 == 0) ? 9 : num % 9;
+    }
+}
