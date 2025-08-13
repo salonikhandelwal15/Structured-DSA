237e30ccbe2c0ea11762d36986070369c8729d6a
Saloni <salonikhandelwal22553@gmail.com>
2025-08-13 08:52:35 +0530

    Solved the problem to move all 0's to the end while maintaining the relative order of the non-zero elements

---
+++ Arrays/src/MoveZeroesAtEnd.java
@@ -0,0 +1,25 @@
+
+public class MoveZeroesAtEnd {
+    public static void main(String[] args) {
+        int[] num = {0, 1, 0, 3, 12};
+        moveZeroes(num);
+
+
+
+        for (int n : num) {
+            System.out.print(n + " ");
+        }
+    }
+
+    public static void moveZeroes(int[] num) {
+        int count = 0;
+        for (int i = 0; i < num.length; i++) {
+            if (num[i] != 0) {
+                num[count++] = num[i];
+            }
+        }
+        while (count < num.length) {
+            num[count++] = 0;
+        }
+    }
+}
