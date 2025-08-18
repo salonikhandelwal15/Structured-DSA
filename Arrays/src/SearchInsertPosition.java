3b0b0d3be822e7ad824ad0d128f1cb895e52e997
Saloni <salonikhandelwal22553@gmail.com>
2025-08-18 15:53:28 +0530

    Solved a problem to search the insert position in the array

---
+++ Arrays/src/SearchInsertPosition.java
@@ -0,0 +1,25 @@
+public class SearchInsertPosition {
+    public static void main(String[] args) {
+        int [] nums={1,3,5,7};
+        int target=5;
+        System.out.println(searchInsert(nums,target));
+
+    }
+    public static int searchInsert(int[]nums,int target){
+        int start=0;
+        int end=nums.length-1;
+        while(start<=end){
+            int mid=(start+end)/2;
+            if(nums[mid]==target){
+                return mid;
+            }else if(nums[mid]>target)
+            {
+                end=mid-1;
+            }
+            else {
+                start=mid+1;
+            }
+        }
+        return start;
+    }
+}
