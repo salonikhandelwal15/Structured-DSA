076ddbb9a3a90f5c34506d7869169366b31051e9
Saloni <salonikhandelwal22553@gmail.com>
2025-08-20 23:29:38 +0530

    Implemented Kadane's Algorithm to solve Maximum SubArray

---
+++ Arrays/src/MaximumSubArray.java
@@ -0,0 +1,24 @@
+public class MaximumSubArray {
+    public static void main(String[] args) {
+        int[] nums={-2,1,-3,4,-1,2,1,-5,4};
+        System.out.println(maxSumArr(nums));
+
+    }
+    public static int maxSumArr(int[] nums){
+        int sum=0;
+        int max_sum=nums[0];
+
+        for(int i=0;i<nums.length;i++){
+            sum=sum+nums[i];
+            if(sum>max_sum)
+            {
+                max_sum=sum;
+            }
+            if(sum<0)
+            {
+                sum=0;
+            }
+        }
+        return max_sum;
+    }
+}
