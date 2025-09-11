e3c295bddd3dff42dbcaac89b49a8ef49adc8568
Saloni <salonikhandelwal22553@gmail.com>
2025-09-11 20:18:13 +0530

    Solved the problem to Find the Length of Last Word

---
+++ Strings/src/LengthOfLastWord.java
@@ -0,0 +1,24 @@
+public class LengthOfLastWord {
+    public static void main(String[] args) {
+        String str="fly to the moon";
+        LengthOfLastWord(str);
+        System.out.println(LengthOfLastWord(str));
+
+    }
+    static int LengthOfLastWord(String s){
+        String str=s.trim();
+        int count=0;
+        for(int i=str.length()-1;i>=0;i--)
+        {
+            if(str.charAt(i)!=' ')
+            {
+                count++;
+
+            }
+            else{
+                break;
+            }
+        }
+        return count;
+    }
+}
